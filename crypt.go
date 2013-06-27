6ca58109a6d99ca4e780ab9ca89fea76039d222f
Jonas mg <jonas@matx.cc>
2013-06-27 10:29:39 +0100

    Move functions of package common to main package

---
+++ crypt.go
@@ -0,0 +1,5 @@
+// (C) Copyright 2012, Jeramey Crawford <jeramey@antihe.ro>. All
+// rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package crypt
