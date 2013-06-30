6fd8807637bbedad651e49b2a08dc23e0915d2b3
Jonas mg <jonas@matx.cc>
2013-06-30 11:06:05 +0100

    Add Authors file

--- crypt.go
+++ crypt.go
@@ -1,4 +1,4 @@
-// (C) Copyright 2013, Jonas mg <jonas.mg@sent.at>. All rights reserved.
+// (C) Copyright 2013, Jonas mg. All rights reserved.
 // Use of this source code is governed by a BSD-style license
 // that can be found in the LICENSE file.
 
