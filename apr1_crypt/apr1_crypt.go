c12518bb8b632eb5124f0f28beee906e18af4920
Kohei YOSHIDA <kohei.yoshida@gehirn.co.jp>
2017-01-14 11:40:51 +0900

    apr1_crypt: return md5_crypt directly

--- apr1_crypt/apr1_crypt.go
+++ apr1_crypt/apr1_crypt.go
@@ -26,30 +26,14 @@ const (
 	RoundsDefault = 1000
 )
 
-var md5Crypt = md5_crypt.New()
-
-func init() {
-	md5Crypt.SetSalt(common.Salt{
+// New returns a new crypt.Crypter computing the variant "apr1" of MD5-crypt
+func New() crypt.Crypter {
+	crypter := md5_crypt.New()
+	crypter.SetSalt(common.Salt{
 		MagicPrefix:   []byte(MagicPrefix),
 		SaltLenMin:    SaltLenMin,
 		SaltLenMax:    SaltLenMax,
 		RoundsDefault: RoundsDefault,
 	})
+	return crypter
 }
-
-type crypter struct{ Salt common.Salt }
-
-// New returns a new crypt.Crypter computing the variant "apr1" of MD5-crypt
-func New() crypt.Crypter { return &crypter{common.Salt{}} }
-
-func (c *crypter) Generate(key, salt []byte) (string, error) {
-	return md5Crypt.Generate(key, salt)
-}
-
-func (c *crypter) Verify(hashedKey string, key []byte) error {
-	return md5Crypt.Verify(hashedKey, key)
-}
-
-func (c *crypter) Cost(hashedKey string) (int, error) { return RoundsDefault, nil }
-
-func (c *crypter) SetSalt(salt common.Salt) {}
